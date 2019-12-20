f057d4df00c5d402a31fe5b7e4166ef4eb9ca992
Matty Watts <watts.mathew@gmail.com>
2019-12-20 00:07:55 -0600

    fixing employee.js

--- app.js
+++ app.js
@@ -2,9 +2,34 @@ const Engineer = require('./lib/engineer');
 const Intern = require('./lib/intern');
 const Manager = require('./lib/manager');
 const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util')
+
+const appendFileAsync = util.promisify(fs.appendFile);
 
 
 const startApp = function startApp() {
+    appendFileAsync('./output/team.html', `
+        <!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>Development Team Roster</title>
+</head>
+
+<body>
+    <h1>Your Team</h1>
+    <div class="container">
+        `)
+
     inquirer.prompt([
         {
             type: 'input',
